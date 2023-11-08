ffa2e69a7abd12b9ca3477e66161a06b988b9e0d
TamannaTahsin1 <tamannatahsin648@gmail.com>
2023-11-08 19:20:02 +0600

    sorting by date

--- src/Pages/AvailableFoods/AvailableFoods.jsx
+++ src/Pages/AvailableFoods/AvailableFoods.jsx
@@ -3,12 +3,16 @@ import { useLoaderData, useParams } from "react-router-dom";
 import AvailableFoodsCard from "./AvailableFoodsCard";
 
 const AvailableFoods = () => {
+  const foods = useLoaderData()
+  // console.log(foods)
+  const {food_name} = useParams()
+  // console.log(food_name)
     const [availableFoods, setAvailableFoods] = useState([])
-    const {food_name} = useParams()
-    // console.log(food_name)
-    const foods = useLoaderData()
-    // console.log(foods)
-
+    // sorting
+    const handleSortByDate = () =>{
+      const sortedFoods = [...foods].sort((x,y) => x.date - y.date)
+      setAvailableFoods(sortedFoods)
+    }
     // sideEffect
     useEffect(() =>{
         const findFoods = foods?.filter(food => food.food_name == food_name)
@@ -21,6 +25,9 @@ const AvailableFoods = () => {
       <h3 className='text-4xl font-extrabold bg-gradient-to-r from-yellow-100 via-yellow-500 to-gray-300 text-transparent bg-clip-text text-center mb-2'>
         Available Foods
       </h3>
+  <div className="p-5">
+  <button onClick={handleSortByDate} className="btn border-none bg-gradient-to-r from-yellow-200 to-yellow-600 hover:from-red-500 hover:to-orange-500 text-white">Sort by Date</button>
+  </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         {
             availableFoods?.map(availableFood => <AvailableFoodsCard key={availableFood._id} availableFood={availableFood}></AvailableFoodsCard>)
